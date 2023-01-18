0b25a87c8ac4b2a1222d8941174b5acb8ed77d82
Nikita Smagin <n.smagin@agimagroup.ru>
2023-01-18 11:19:04 +0500

    fix order form

--- components/MainPage/OrderForm.tsx
+++ components/MainPage/OrderForm.tsx
@@ -1,76 +1,101 @@
 import Image from 'next/image'
-import type { FC } from 'react'
+import type { FC, FormEvent } from 'react'
 import React from 'react'
 
+import WtAutoComplete from '../UI/WtAutoComplete/WtAutoComplete'
 import WTButton from '../UI/WTButton/WTButton'
-import WTInput from '../UI/WtInput/WTInput'
+import { WTDatePicker } from '../UI/WTDatepicker/WTDatePicker'
 
 type FormTypes = {
-    direction: string
-    setDirection: (value: string) => void
+    directionFrom: string
+    setDirectionFrom: (value: string) => void
+    directionTo: string
+    setDirectionTo: (value: string) => void
+    startDate: Date | null
+    endDate: Date | null
+    setStartDate: (value: Date) => void
+    setEndDate: (value: Date) => void
+    onSubmit: (e: FormEvent<HTMLFormElement>) => void
+    row?: boolean
 }
 
-const OrderForm: FC<FormTypes> = ({ direction, setDirection }) => {
+const OrderForm: FC<FormTypes> = ({
+    directionFrom,
+    directionTo,
+    setDirectionFrom,
+    setDirectionTo,
+    setEndDate,
+    setStartDate,
+    startDate,
+    endDate,
+    onSubmit,
+    row,
+}) => {
+    const changeCities = () => {
+        setDirectionTo(directionFrom)
+        setDirectionFrom(directionTo)
+    }
     return (
-        <form className="flex min-w-[730px] flex-col bg-black-80 px-5 py-[60px]">
-            <span className="mb-[7px] text-3xl font-light text-white">
-                Направление
-            </span>
-            <div className="mb-[60px] flex items-center gap-2">
-                <WTInput
-                    value={direction}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setDirection(e.target.value)
-                    }
-                    className={''}
-                    placeholder="Откуда"
-                    icon="direction"
-                    full
-                />
-                <Image
-                    src={'/icons/icon-circle.svg'}
-                    alt={'svg'}
-                    width={24}
-                    height={24}
-                    className="shrink-0"
-                />
-                <WTInput
-                    value={direction}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setDirection(e.target.value)
-                    }
-                    className={''}
-                    placeholder="Откуда"
-                    icon="direction"
-                    full
-                />
+        <form
+            className={`mx-auto flex min-w-[730px] max-w-[1490px] flex-col bg-black-80 px-5  ${
+                row ? 'pt-[28px] pb-[76px]' : 'py-[60px]'
+            }`}
+            onSubmit={(e) => onSubmit(e)}
+        >
+            <div
+                className={`flex w-full ${
+                    row ? 'mb-[47px] flex-row' : 'mb-[92px] flex-col'
+                }`}
+            >
+                <div
+                    className={`flex flex-col ${
+                        row ? 'mr-[35px] grow' : 'mb-[60px]'
+                    }`}
+                >
+                    <span className="mb-[7px] block text-3xl font-light text-white">
+                        Направление
+                    </span>
+                    <div className="flex items-center gap-2">
+                        <WtAutoComplete full placeholder="Куда" />
+
+                        <button
+                            className={'shrink-0'}
+                            onClick={changeCities}
+                            type={'button'}
+                        >
+                            <Image
+                                src={'/icons/icon-circle.svg'}
+                                alt={'svg'}
+                                width={24}
+                                height={24}
+                                className="shrink-0"
+                            />
+                        </button>
+                        <WtAutoComplete full placeholder="Откуда" />
+                    </div>
+                </div>
+
+                <div className={'grow flex-col'}>
+                    <span className="mb-[7px] block text-3xl font-light text-white">
+                        Дата
+                    </span>
+                    <div className="flex items-center gap-10">
+                        <WTDatePicker
+                            value={startDate}
+                            onChange={(date: Date) => setStartDate(date)}
+                        />
+                        <WTDatePicker
+                            value={endDate}
+                            onChange={(date: Date) => setEndDate(date)}
+                        />
+                    </div>
+                </div>
             </div>
-            <span className="mb-[7px] text-3xl font-light text-white">
-                Дата
-            </span>
-            <div className="mb-[92px] flex items-center gap-10">
-                <WTInput
-                    value={direction}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setDirection(e.target.value)
-                    }
-                    className={''}
-                    placeholder="ДД/ММ/ГГ"
-                    icon="date"
-                    full
-                />
-                <WTInput
-                    value={direction}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setDirection(e.target.value)
-                    }
-                    className={''}
-                    placeholder="ДД/ММ/ГГ"
-                    icon="date"
-                    full
-                />
-            </div>
-            <WTButton className="self-end rounded-lg py-[18px] px-[68px] text-2xl ">
+
+            <WTButton
+                type={'submit'}
+                className="self-end rounded-lg py-[18px] px-[70px] text-2xl "
+            >
                 НАЙТИ БИЛЕТЫ
             </WTButton>
         </form>
---
+++ components/OrderLayout/OrderHeader.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import type { FormEvent } from 'react'
+import React, { useState } from 'react'
+
+import OrderForm from '../MainPage/OrderForm'
+
+const OrderHeader = () => {
+    const [directionFrom, setDirectionFrom] = useState('')
+    const [directionTo, setDirectionTo] = useState('')
+    const [startDate, setStartDate] = useState<Date | null>(null)
+    const [endDate, setEndDate] = useState<Date | null>(null)
+    const router = useRouter()
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        router.push('/order')
+    }
+    return (
+        <header className="bg-black relative z-0 mb-[205px] pt-[400px]">
+            <div
+                className="absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat"
+                style={{ backgroundImage: "url('/order-bg.png')" }}
+            />
+            <div>
+                <OrderForm
+                    directionFrom={directionFrom}
+                    setDirectionFrom={setDirectionFrom}
+                    directionTo={directionTo}
+                    setDirectionTo={setDirectionTo}
+                    setEndDate={setEndDate}
+                    setStartDate={setStartDate}
+                    startDate={startDate}
+                    endDate={endDate}
+                    onSubmit={onSubmit}
+                    row
+                />
+            </div>
+        </header>
+    )
+}
+
+export default OrderHeader
